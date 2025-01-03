4a3c609b03023dd89396f1fafc9745b265d52995
Seong Heung Bae <mopil1102@gmail.com>
2025-01-03 09:36:04 +0900

    feat: cors 설정 추가

---
+++ src/main/kotlin/com/project/youthmoa/api/configuration/WebConfiguration.kt
@@ -0,0 +1,15 @@
+package com.project.youthmoa.api.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfiguration : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowCredentials(true)
+    }
+}
