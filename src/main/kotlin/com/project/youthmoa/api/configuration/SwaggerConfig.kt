abd6a66a65e73c321af3bb883787fd48def29411
Seong Heung Bae <mopil1102@gmail.com>
2025-02-02 01:12:14 +0900

    feat: swagger server url 추가

--- src/main/kotlin/com/project/youthmoa/api/configuration/SwaggerConfig.kt
+++ src/main/kotlin/com/project/youthmoa/api/configuration/SwaggerConfig.kt
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
 import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -24,6 +25,12 @@ class SwaggerConfig {
     fun openAPI(): OpenAPI {
         return OpenAPI()
             .components(Components())
+            .servers(
+                listOf(
+                    Server().url("http://localhost:8080").description("local"),
+                    Server().url("https://youth-moa-server.click").description("live"),
+                ),
+            )
             .info(apiInfo())
     }
 
