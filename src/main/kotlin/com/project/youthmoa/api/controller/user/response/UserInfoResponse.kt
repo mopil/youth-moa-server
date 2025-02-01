d5f58e3140cdbbf704f8c493734e872103edad93
Seong Heung Bae <mopil1102@gmail.com>
2025-02-02 01:33:30 +0900

    feat: 유저조회 응답필드 추가

--- src/main/kotlin/com/project/youthmoa/api/controller/user/response/UserInfoResponse.kt
+++ src/main/kotlin/com/project/youthmoa/api/controller/user/response/UserInfoResponse.kt
@@ -2,11 +2,14 @@ package com.project.youthmoa.api.controller.user.response
 
 import com.project.youthmoa.domain.model.User
 import com.project.youthmoa.domain.type.Gender
+import com.project.youthmoa.domain.type.UserRole
 import java.time.LocalDate
 import java.time.LocalDateTime
 
 data class UserInfoResponse(
     val id: Long,
+    val email: String,
+    val role: UserRole,
     val name: String,
     val phone: String,
     val address: String,
@@ -19,6 +22,8 @@ data class UserInfoResponse(
         fun from(user: User) =
             UserInfoResponse(
                 id = user.id,
+                email = user.email,
+                role = user.role,
                 name = user.name,
                 phone = user.phone,
                 address = user.address,
